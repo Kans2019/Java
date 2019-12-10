4f899e1c2748ce77c473dd91d2e11ed800b9747a
Kans924 <kanghai_924@163.com>
2019-12-11 00:56:01 +0800

    optional

---
+++ src/main/java/com/kanghai/java8/chapter10/optinaltest/optionaltest.java
@@ -0,0 +1,15 @@
+package com.kanghai.java8.chapter10.optinaltest;
+
+/**
+ * @author likanghai
+ * @version 1.0
+ * @date 2019/12/11 0:30
+ * @description
+ */
+
+public class optionaltest {
+
+    public static void main(String[] args) {
+
+    }
+}
